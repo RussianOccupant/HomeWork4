51c07d1e5ff6c43f1aaaf82bcb9aa93898cae688
AlexBystrov <sshbystrov@gmail.com>
2023-01-25 22:02:40 +0400

    Added answer for exersice with *

---
+++ Exersice4/Program.cs
@@ -0,0 +1,28 @@
+﻿int Star(int M)
+{
+
+    for (int i = 1; i <= M; i++)
+    {
+        for (int m = 0; m < i; m++)
+        {
+            Console.Write("*");
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+
+
+    return M;
+}
+
+void Main()
+{
+    Console.Clear();
+    Console.Write("Введите высоту елки: ");
+    int h = int.Parse(Console.ReadLine()!);
+    int M = Star(h);
+}
+Main();
+
+
+
