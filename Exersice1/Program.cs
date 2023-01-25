e16c24024453b1f7c888bb43cff127c2b44cfc2f
AlexBystrov <sshbystrov@gmail.com>
2023-01-25 21:58:57 +0400

    Added answer for exersice 25

---
+++ Exersice1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В) 
+// и возводит число А в натуральную степень В
+int GetStep(int M)
+{   
+    Console.Write("Enter number B: ");
+    int B = int.Parse(Console.ReadLine()!);
+    int C = M;
+    int count = 1;
+    while (count < B)
+    {
+        M = M * C;
+        count++;
+    }
+    return M;
+}
+
+void Main()
+{
+Console.Clear();
+Console.Write("Enter number A: ");
+int A = int.Parse(Console.ReadLine()!);
+int M = GetStep(A);
+Console.WriteLine(M);
+}
+Main();
